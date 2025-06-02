c9553779eb69e66700680a72bdaf25ca15342238
AbhijeetKumarGupta <abhijeetg375@gmail.com>
2025-06-02 11:27:50 +0530

    chore: project name update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 echo "Building .dacpac"
-dotnet build DatabaseProjecthands.sqlproj
+dotnet build TSQLPOC.sqlproj
 echo ".dacpac built successfully"
 
 echo "Deploying Project"
 sqlpackage /Action:Publish \
-    /SourceFile:bin/Debug/DatabaseProjecthands.dacpac \
+    /SourceFile:bin/Debug/TSQLPOC.dacpac \
     /TargetServerName:localhost,1433 \
     /TargetDatabaseName:hands \
     /TargetUser:sa \
